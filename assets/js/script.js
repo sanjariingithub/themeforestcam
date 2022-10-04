5a71cddaf34f4a9233aec45c59c7a1a898afec6e
sanjariingithub <sanjarir018@gmail.com>
2022-10-04 22:12:58 +0600

    first-commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,27 @@ jQuery(function ($) {
 
     'use strict';
 
-    // Hot Deals Carousel starts
+    // hero Carousel
+
+    $('.hero-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        dots: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+    // Hot Deals Carousel ends
+
     $('.hot-deals__carousel').owlCarousel({
         loop: true,
         margin: 10,
@@ -12,31 +32,16 @@ jQuery(function ($) {
             0: {
                 items: 1
             },
+            300:{
+                items: 2
+            },
             600: {
-                items: 3
+                items: 2
             },
             1000: {
                 items: 3
             }
         }
     })
-    // Hot Deals Carousel ends
-
-    $('.owl-three').owlCarousel({
-        loop:false,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:0
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:4
-            }
-        }
-    });
 
 });
